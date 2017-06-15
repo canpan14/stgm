bda5aa3c2e758e5c93f71bfedd6c638392440b3d
canpan14 <sammachlin@hotmail.com>
2017-06-14 22:05:13 -0400

    Board generates tiles that cards can be dropped into.

--- SingleplayerTheMultiplayerGame/Assets/Scripts/Board.cs
+++ SingleplayerTheMultiplayerGame/Assets/Scripts/Board.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 namespace STMG {
     public class Board : MonoBehaviour {
+        public GameObject tiles;
         public int width;
         public int height;
 
@@ -15,6 +16,25 @@ namespace STMG {
             for (int i = 0; i < gameBoard.GetLength(0); i++) {
                 for (int j = 0; j < gameBoard.GetLength(1); j++) {
                     gameBoard[j, i] = new BoardTile();
+                }
+            }
+        }
+
+        private void Start() {
+            RectTransform boardRect = (RectTransform)this.transform;
+            float xSpacing = boardRect.rect.width / width;
+            float ySpacing = boardRect.rect.height / height;
+            float startingXLocation = -2*xSpacing;
+            float startingYLocation = 2*ySpacing;
+            for(int i = 0; i < 5; i++) {
+                float newYLocation = startingYLocation - ySpacing * i;
+                for (int j = 0; j < 5; j++) {
+                    float newXLocation = startingXLocation + xSpacing * j;
+                    GameObject tile = (GameObject)Instantiate(tiles);
+                    tile.transform.parent = this.gameObject.transform;
+                    tile.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+                    //tile.GetComponent<RectTransform>().localPosition = new Vector3(startingXLocation, startingYLocation, 0);
+                    tile.GetComponent<RectTransform>().localPosition = new Vector3(newXLocation, newYLocation, 0);
                 }
             }
         }
--- SingleplayerTheMultiplayerGame/Assets/Scripts/Draggable.cs
+++ SingleplayerTheMultiplayerGame/Assets/Scripts/Draggable.cs
@@ -37,7 +37,7 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
         int newSiblingIndex = placeholderParent.childCount;
 
         for(int i = 0; i < placeholderParent.childCount; i++) {
-            if(this.transform.position.x < placeholderParent.GetChild(i).position.x) {
+            if(this.transform.position.y > placeholderParent.GetChild(i).position.y) {
                 newSiblingIndex = i;
                 if(placeholder.transform.GetSiblingIndex() < newSiblingIndex) {
                     newSiblingIndex--;
